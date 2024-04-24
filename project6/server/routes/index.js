ef2d6a27184277b4ee49322371b064127b3e2a20
joelward2025 <joelward@cedarville.edu>
2024-04-23 20:35:44 -0400

    disables CORS

--- project6/server/routes/index.js
+++ project6/server/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var db = require('../db/database.js');
 var router = express.Router();
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /* GET home page. */
 router.get('/getCombined', function(req, res, next) {
