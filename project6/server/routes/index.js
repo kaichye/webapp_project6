5d84031801d9d88972a71954be57557db1f8394f
joelward2025 <joelward@cedarville.edu>
2024-04-23 20:08:24 -0400

    express can show notes

--- project6/server/routes/index.js
+++ project6/server/routes/index.js
@@ -17,4 +17,21 @@ router.get('/getCombined', function(req, res, next) {
   });
 });
 
+/* GET notes. */
+// get notes at http://localhost:3000/getNotes?id=3
+router.get('/getNotes', function(req, res, next) {
+  var sql = "SELECT * from note where ownerId = ";
+  sql += req.query.id;
+  db.query(sql, (err, rows) => {
+  
+    if(err){
+      console.log("SELECT from user failed");
+      console.log(err);
+      return;
+    }
+    //Render index.pug page using array
+    res.render('getNotes', {notes: rows});
+  }); 
+});
+
 module.exports = router;
